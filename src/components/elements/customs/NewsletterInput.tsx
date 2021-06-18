4991356cb48b00af9e97dc85ec48cfa337d69453
MateusOli21 <mateus.oli.abreu@hotmail.com>
2021-06-17 22:24:42 -0300

    adjust: responsiveness to newsletter input component.

--- src/components/elements/customs/NewsletterInput.tsx
+++ src/components/elements/customs/NewsletterInput.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { PrimaryButton } from '../buttons';
 
 const NewsletterInput: React.FC = () => (
-  <div className="mt-9 p-4 bg-white rounded">
+  <div className="flex flex-col items-center md:flex-row mt-9 p-4 md:bg-white rounded">
     <input
       placeholder="Seu melhor e-mail"
-      className="py-2 px-2 mr-1 focus:outline-none"
+      className=" w-full py-3 px-4 mb-4 rounded md:rounded-none md:mb-0 md:py-2 md:px-2 md:mr-1 focus:outline-none"
     />
     <PrimaryButton>Assinar</PrimaryButton>
   </div>
