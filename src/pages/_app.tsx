c031a511cb46b0939cb2477235e465c667d7aded
MateusOli21 <mateus.oli.abreu@hotmail.com>
2021-06-19 13:04:22 -0300

    adjust: import from tailwind.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
-
 import { ApolloProvider } from '@apollo/client';
-import { apolloClient } from '@providers/graphql';
-import GlobalStyles from '../styles/globals';
-import { theme } from '../styles/themes/main';
 
+import { apolloClient } from '@providers/graphql';
+import GlobalStyles from '@styles/globals';
+import { theme } from '@styles/themes/main';
+
+import 'tailwindcss/tailwind.css';
 import '../styles/tailwind.css';
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => (
   <ThemeProvider theme={theme}>
     <ApolloProvider client={apolloClient}>
-      <GlobalStyles />
       <Component {...pageProps} />
+      <GlobalStyles />
     </ApolloProvider>
   </ThemeProvider>
 );
